f4dcf79147cedcf69cd8a782878da91d192a3e4a
Destian Rezky Pratama <reskigantengmunyuk123@gmail.com>
2026-09-22 11:47:36 +0700

    Mengubah nama profil dan menambahkan foto user

--- config.php
+++ config.php
@@ -21,7 +21,7 @@ error_reporting(0);
 	// sesuaikan dengan server anda
 	$host 	= 'localhost'; // host server
 	$user 	= 'root';  // username server
-	$pass 	= ''; // password server, kalau pakai xampp kosongin saja
+	$pass 	= 'root'; // password server, kalau pakai xampp kosongin saja
 	$dbname = 'db_toko'; // nama database anda
 	
 	try {
